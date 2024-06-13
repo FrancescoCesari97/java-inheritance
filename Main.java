9b2c2477fb505e727dff73e86520399734c5a771
FrancescoCesari97 <freedus97@gmail.com>
2024-06-13 12:48:10 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+
+        car.go();
+
+        Bicycle bicycle = new Bicycle();
+
+        bicycle.stop();
+
+        System.out.println(car.doors);
+
+    }
+
+}
---
+++ Vehicle.java
@@ -0,0 +1,13 @@
+public class Vehicle {
+
+    double speed;
+
+    void go() {
+        System.out.println("this veicle is moving");
+    }
+
+    void stop() {
+        System.out.println("this veicle is stopped");
+    }
+
+}
